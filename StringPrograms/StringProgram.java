701f5450862e1c4cfa001dc1c7c4238c470f52cb
SurendraTeja <100977779+SurendraTeja@users.noreply.github.com>
2022-03-05 07:12:56 +0530

    Add files via upload

---
+++ StringPrograms/StringProgram.java
@@ -0,0 +1,38 @@
+class StringReverseProgram
+{	
+	String name;
+	char[] revstr;
+	public char[] getName()
+	{
+		return this.revstr;
+	}
+	public void setName(String name)
+	{
+		revstr=new char[name.length()];
+		int j=0;
+		for(int i=name.length()-1;i>=0;i--)
+		{
+			revstr[j]=name.charAt(i);
+			++j;
+		}
+		this.revstr=revstr;
+	}
+	public void method()
+	{
+		System.out.println("The Reverse String is :");
+		for(char a:revstr)
+		{
+			System.out.print(a);
+		}
+	}
+}
+class StringProgram
+{
+	public static void main(String[] args)
+	{
+		String s="SURENDRA";
+		StringReverseProgram obj=new StringReverseProgram();
+						  obj.setName(s);
+						  obj.method();
+	}
+}
